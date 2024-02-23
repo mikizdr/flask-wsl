2072cec99d83832f3e891693ede6816afef080ec
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2024-02-23 18:49:11 +0100

    User model

---
+++ shopping/models/user.py
@@ -0,0 +1,13 @@
+from shopping import db
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email = db.Column(db.String(length=50), nullable=False, unique=True)
+    email_verified_at = db.Column(db.DateTime(), nullable=True)
+    password = db.Column(db.String(length=60), nullable=False)
+    remember_token = db.Column(db.String(length=100), nullable=True)
+
+    def __repr__(self):
+        return f"User('{self.id}')"
