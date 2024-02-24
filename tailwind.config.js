4564fc5500cce174bc56767a0f67de87b9e8c9f7
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2024-02-24 21:31:27 +0100

    Compiled Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./shopping/templates/**/*.html",
+      "./shopping/static/src/**/*.js"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
