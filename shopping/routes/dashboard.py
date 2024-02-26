05d5f05bcb8a831dea87caac7dc057fb429ccbe6
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2024-02-26 18:51:02 +0100

    Add login manager login view and login_required decorator

--- shopping/routes/dashboard.py
+++ shopping/routes/dashboard.py
@@ -3,6 +3,8 @@ from flask import (
     render_template,
 )
 
+from flask_login import login_required
+
 bp = Blueprint("dashboard", __name__)
 
 @bp.route("/")
@@ -10,5 +12,6 @@ def index() -> str:
     return render_template("welcome.html")
 
 @bp.route("/home")
+@login_required
 def home() -> str:
     return render_template("dashboard/index.html")
