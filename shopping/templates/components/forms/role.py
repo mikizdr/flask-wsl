178915bc62c160feb2ae4fe221679ffc5c9816b1
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2024-02-28 21:01:41 +0100

    Add validation for role name in RoleForm

--- shopping/templates/components/forms/role.py
+++ shopping/templates/components/forms/role.py
@@ -1,11 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField, ValidationError
 from wtforms.validators import DataRequired
+
+from shopping.models.definitions import Role
 
 
 class RoleForm(FlaskForm):
+
+    def validate_name(self, name_to_check) -> None:
+        user: Role | None = Role.query.filter_by(name=name_to_check.data).first()
+        if user:
+            raise ValidationError(
+                "This role already exists! Please try a different name."
+            )
+
     name = StringField(label="Role Name:", validators=[DataRequired()])
-    description = StringField(label="Description:", validators=[DataRequired()])
+    description = TextAreaField(label="Description:", validators=[DataRequired()])
     submit = SubmitField(label="Create Role")
 
     def __repr__(self) -> str:
