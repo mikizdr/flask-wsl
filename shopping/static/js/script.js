cc9c3e7ee99b88682e7fa9b123007b62b9f9064e
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2024-03-02 23:06:47 +0100

    Add popup-info-modal component and update script.js

--- shopping/static/js/script.js
+++ shopping/static/js/script.js
@@ -6,3 +6,16 @@ document.addEventListener('DOMContentLoaded', function () {
         }, 5000);
     }
 }, false);
+
+function changeIconColor(green = true) {
+    const svg = document.getElementById('svgPopupIcon');
+    if (green) {
+        svg.classList.remove('text-red-400', 'dark:text-red-400');
+        svg.classList.add('text-green-400', 'dark:text-green-400');
+    } else {
+        svg.classList.remove('text-green-400', 'dark:text-green-400');
+        svg.classList.add('text-red-400', 'dark:text-red-400');
+    }
+}
+
+// Path: shopping/static/js/script.js
