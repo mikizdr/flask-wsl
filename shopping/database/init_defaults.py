348938598489f3c7f9cafc541e47f9452ae16051
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2024-03-04 22:16:23 +0100

    Add pre-population of Product Category table

--- shopping/database/init_defaults.py
+++ shopping/database/init_defaults.py
@@ -1,5 +1,5 @@
 from shopping import db
-from shopping.models.definitions import Role, User
+from shopping.models.definitions import Category, Role, User
 from werkzeug.security import generate_password_hash
 
 
@@ -21,4 +21,17 @@ def init_defaults():
         user = User(username='admin', email='admin@email.com', password=generate_password_hash('admin'), role_id=1)
         db.session.add(user)
         db.session.commit()
+        
+    """Pre-Populate Product Category Table"""
+    if Category.query.first() is None:
+        catogories: list[dict[str, str]] = [
+            {"name": "Vegetable", "description": "Different types of vegetables"},
+            {"name": "Fruit", "description": "Different types of fruits"},
+            {"name": "Cereals", "description": "Different types of cereals"},
+        ]
+        for category in catogories:
+            product_catogory = Category(name=category['name'], description=category['description'])
+            db.session.add(product_catogory)
+        db.session.commit()
+
     pass
