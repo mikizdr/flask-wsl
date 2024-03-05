84df157bf2301cc17518cc7fee507d5485f7501c
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2024-03-05 23:13:17 +0100

    Add error handling and instructions for changing the port number

--- run.py
+++ run.py
@@ -2,4 +2,12 @@ from shopping import app
 
 # Compare this snippet from shopping/__init__.py:
 if __name__ == "__main__":
-    app.run(debug=True)
+    """
+    If the next error occurs:
+    socket.error: [Errno 10013] An attempt was made to access a socket in a way forbidden by its access permissions
+    then, put the port number to some other value than 5000, for example 8000. Port can be get from .env file or
+    from the configuration file.
+    """
+    app.run(
+        debug=True, port=5000
+    )  # Run the app in debug mode (for development purposes only!)
