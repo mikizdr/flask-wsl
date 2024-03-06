7589c6b0cb15f45d1ee6823ed66b1a5fa497cbc0
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2024-03-06 11:09:24 +0100

    Add resource update and remove functions in script.js

--- shopping/static/js/script.js
+++ shopping/static/js/script.js
@@ -76,5 +76,107 @@ function showHideValidationMessages(form, formFields) {
     });
 }
 
+function setResourceIdAndType(button) {
+    document.getElementById('resourceIdAndType').value = JSON.stringify({ resourceId: button.dataset.resourceId, type: button.dataset.type });
+}
+
+/* Variables that hold the old state of resource name and description. */
+let oldName = '';
+let oldDescription = '';
+
+/**
+* Update resource
+*
+* @param {HTMLElement} element - the button element
+* @param {boolean} update - if true, update the resource
+*
+* @returns {void}
+*/
+function updateResource(element, update = false) {
+    const afterActionMessageInPopupModal = document.getElementById('afterActionMessageInPopupModal');
+    const row = element.closest('tr');
+
+    element.style.display = 'none'
+    element.nextElementSibling.style.display = 'block';
+
+    const id = element.dataset.resourceId;
+    const resourceType = element.dataset.type;
+    const name = row.querySelector('td:nth-child(3) div').innerText;
+    const description = row.querySelector('td:nth-child(4) div')
+
+
+    if (!update) {
+        oldName = name;
+        oldDescription = description.innerText;
+    }
+
+    !update && description.focus();
+
+    if (update) {
+        fetch(`/${resourceType}/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: name,
+                description: description.innerText
+            })
+        })
+            .then(response => {
+                if (response.status === 200) {
+                    changeIconColor();
+                    afterActionMessageInPopupModal.innerText = `${resourceType.capitalize()} updated successfully.`;
+                } else {
+                    name.innerText = oldName;
+                    description.innerText = oldDescription;
+                    changeIconColor(false);
+                    afterActionMessageInPopupModal.innerText = `${resourceType.capitalize()} cannot be updated.`;
+                }
+                const button = document.getElementById('showHidePopupModal');
+                button.click();
+                element.previousElementSibling.style.display = 'block';
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+}
+
+/**
+ * Remove resource
+ *
+ * @returns {void}
+ */
+function removeResource() {
+    const resourceData = document.getElementById('resourceIdAndType').value;
+    const id = JSON.parse(resourceData).resourceId;
+    const resourceType = JSON.parse(resourceData).type;
+
+    const button = document.querySelector(`[data-resource-id="${id}"]`);
+    row = button.closest('tr');
+
+    fetch(`/${resourceType}/${id}`, {
+        method: 'DELETE'
+    })
+        .then(response => {
+            if (response.status === 200) {
+                afterActionMessageInPopupModal.innerText = `${resourceType.capitalize()} deleted successfully.`;
+                changeIconColor();
+                row.remove();
+            } else {
+                changeIconColor(false);
+                const message = `${resourceType.capitalize()} cannot be deleted. There are records associated with this ${resourceType}.`;
+                document.getElementById('resourceCantBeDeletedMessage').value = message;
+                afterActionMessageInPopupModal.innerText = message;
+            }
+            const button = document.getElementById('showHidePopupModal');
+            button.click();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
 
 // Path: shopping/static/js/script.js
