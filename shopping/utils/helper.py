fcb64f33fa41e79caa9a07969295cb96c8c3aceb
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2024-03-12 13:25:53 +0100

    Add pagination support to helper.py

--- shopping/utils/helper.py
+++ shopping/utils/helper.py
@@ -4,6 +4,8 @@ Global helper functions, constants and other entities for the app.
 
 import uuid
 
+from flask_sqlalchemy.pagination import Pagination
+
 from shopping import app
 
 
@@ -20,3 +22,13 @@ def make_unique_image_name(filename) -> str:
     extension: str = filename.rsplit(".", 1)[1]
 
     return str(uuid.uuid4()) + "." + extension
+
+
+def paginator(model: object, page: int) -> Pagination:
+    """Return a paginator object."""
+    per_page = app.config["PER_PAGE"]
+    max_per_page = app.config["MAX_PER_PAGE"]
+
+    return model.query.paginate(
+        page=page, per_page=per_page, max_per_page=max_per_page, error_out=False
+    )
