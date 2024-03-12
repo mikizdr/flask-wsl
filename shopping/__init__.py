ffabe7d042197db616f4ca04adf2a776f4b76a2d
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2024-03-12 18:21:32 +0100

    Remove unused code and update port handling in run.py and __init__.py

--- shopping/__init__.py
+++ shopping/__init__.py
@@ -42,3 +42,18 @@ with app.app_context():
 # with open("./shopping/database/dump.sql", "w") as f:
 #     for line in con.iterdump():
 #         f.write("%s\n" % line)
+
+
+PORT = app.config["PORT"]
+
+# Compare this snippet from shopping/__init__.py:
+if __name__ == "__main__":
+    """
+    If the next error occurs:
+    socket.error: [Errno 10013] An attempt was made to access a socket in a way forbidden by its access permissions
+    then, put the port number to some other value than 5000, for example 8000. Port can be get from .env file or
+    from the configuration file.
+    """
+    app.run(
+        debug=True, port=PORT
+    )  # Run the app in debug mode (for development purposes only!)
